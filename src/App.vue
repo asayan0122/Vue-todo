75b5b7f251ad3fa94f3c17682e53c4167280213b
asayan_0122 <k.a.0122@outlook.jp>
2019-11-11 15:04:10 +0900

    Home修正

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,21 @@
       <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
 <style lang="scss">
+body {
+  background: #f6f6f6;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  margin: 0 300px;
 }
 
 #nav {
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <form>
-      <input type="text" v-model="newTodo" placeholder="todo" />
+    <div class="registerBox">
+      <input class="registerBox__input" type="text" v-model="newTodo" placeholder="todo" />
       <br />
-      <button @click="addTodo()">ADD TODO</button>
-    </form>
-    <input type="text" v-model="keyword" placeholder="searchbox" />
-    <div class="todos-container">
+      <button class="registerBox__btn" @click="addTodo()">ADD TODO</button>
+    </div>
+    <div class="searchBox">
+      <i class="fa fa-search searchBox__icon" aria-hidden="true"></i>
+      <input class="searchBox__input" type="text" v-model="keyword" placeholder="searchbox" />
+    </div>
+    <div class="todos-wrap">
       <label
-        class="todos-container__todo"
+        class="todos-wrap__todo"
         v-for="(todo) in searchTodos"
         :key="todo"
         v-bind:class="{ 'todos-container__todo--checked': todo.done }"
@@ -74,8 +77,21 @@ export default {
   display: -ms-flex;
   display: -o-flex;
 }
+.registerBox {
+  width: 300px;
+  height: 40px;
+  margin: 0 auto;
+  border: 1px solid #e8e8e8;
+  border-radius: 20px;
+  margin-bottom: 15px;
+  background: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  box-sizing: border-box;
+}
 
-.todos-container {
+.todos-wrap {
   @include flex-vender;
   flex-direction: column;
   align-items: center;
@@ -88,6 +104,35 @@ export default {
       @extend #{$element};
       color: #85a6c6;
     }
+  }
+}
+input[type="text"]:focus {
+  outline: 0;
+}
+.searchBox {
+  width: 300px;
+  height: 40px;
+  margin: 0 auto;
+  border: 1px solid #e8e8e8;
+  border-radius: 20px;
+  margin-bottom: 15px;
+  background: #fff;
+  display: flex;
+  align-items: center;
+  padding: 0 10px;
+  box-sizing: border-box;
+  &__icon {
+    width: 16px;
+    font-size: 18px;
+    margin-right: 5px;
+  }
+  &__input {
+    width: calc(100% - 30px);
+    height: 100%;
+    border: none;
+    box-sizing: border-box;
+    padding: 5px 10px;
+    font-size: 16px;
   }
 }
 </style>
