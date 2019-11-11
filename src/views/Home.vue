e8a72d347089baab77e6a5f196958ef60662db2c
asayan_0122 <k.a.0122@outlook.jp>
2019-11-11 14:02:11 +0900

    first-commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,93 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <form>
+      <input type="text" v-model="newTodo" placeholder="todo" />
+      <br />
+      <button @click="addTodo()">ADD TODO</button>
+    </form>
+    <input type="text" v-model="keyword" placeholder="searchbox" />
+    <div class="todos-container">
+      <label
+        class="todos-container__todo"
+        v-for="(todo) in searchTodos"
+        :key="todo"
+        v-bind:class="{ 'todos-container__todo--checked': todo.done }"
+      >
+        <input type="checkbox" v-model="todo.done" />
+        <label>{{ todo.text }}</label>
+        <button @click="removeTodo()">削除</button>
+      </label>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  data: function () {
+    return {
+      todos: [
+        { text: 'task', done: false },
+        { text: 'done_task', done: true },
+      ],
+      newTodo: "",
+      keyword: "",
+    }
+  },
+  methods: {
+    addTodo: function () {
+      var text = this.newTodo && this.newTodo.trim()
+      if (!text) {
+        return
+      }
+      this.todos.push({
+        text: text,
+        done: false,
+      })
+      this.newTodo = ''
+    },
+    removeTodo: function () {
+      this.todos.splice(this.todos.indexOf(), 1)
+    }
+  },
+  computed: {
+    searchTodos: function () {
+      var todos = [];
+      for (var i in this.todos) {
+        var todo = this.todos[i];
+        if (todo.text.indexOf(this.keyword) !== -1) {
+          todos.push(todo);
+        }
+      }
+      return todos;
+    }
   }
 }
+
 </script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+@mixin flex-vender() {
+  display: flex;
+  display: -webkit-flex;
+  display: -moz-flex;
+  display: -ms-flex;
+  display: -o-flex;
+}
+
+.todos-container {
+  @include flex-vender;
+  flex-direction: column;
+  align-items: center;
+
+  &__todo {
+    width: 270px;
+    text-align: left;
+    $element: #{&};
+    &--checked {
+      @extend #{$element};
+      color: #85a6c6;
+    }
+  }
+}
+</style>
